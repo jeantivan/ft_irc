c9b9a05cbe494bbc369291f887e24c7df0e5c47f
Jean Tivan <jptivan53@gmail.com>
2026-04-24 16:16:18 +0200

    ADD: Validacion de los argumentos pasados al programa

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
+#include "ft_irc.hpp"
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	if (ac != 3)
+	{
+		std::cerr << "Error: bad params. Usage: " << av[0] << " <port> <password>" << std::endl;
+		return 1;
+	}
+
+	std::string str_port = av[1];
+	std::string password = av[2];
+	int port;
+
+	if (!isValidPort(str_port, port))
+	{
+		std::cerr << "Error: Invalid port value" << std::endl;
+		return 1;
+	}
+
+	std::cout << "WIP: Starting IRC Server on port: " << port << " with password " << password << std::endl;
 	return 0;
 }
