1c028375e5ca031e21ae7341b2be23f17be62ce6
Jean Tivan <jptivan53@gmail.com>
2026-05-21 16:07:35 +0200

    CREATE: Patron Builder para las respuestas del servidor

--- inc/ResponseBuilder.hpp
+++ inc/ResponseBuilder.hpp
@@ -18,6 +18,16 @@ public:
 	ResponseBuilder(const ResponseBuilder &other);
 	~ResponseBuilder();
 	ResponseBuilder &operator=(const ResponseBuilder &other);
+
+	// Build pattern;
+	std::string build();
+	ResponseBuilder &clear();
+	ResponseBuilder &prefix(const std::string &pref);
+	ResponseBuilder &command(const std::string &cmd);
+	ResponseBuilder &params(const std::string &params);
+	ResponseBuilder &trailing(const std::string &trailing);
+	ResponseBuilder &target(const std::string &target);
+	ResponseBuilder &numeric(int code);
 };
 
 #endif // RESPONSEBUILDER_HPP
--- src/ResponseBuilder.cpp
+++ src/ResponseBuilder.cpp
@@ -18,3 +18,83 @@ ResponseBuilder &ResponseBuilder::operator=(const ResponseBuilder &other)
 	}
 	return *this;
 }
+
+ResponseBuilder &ResponseBuilder::clear()
+{
+	prefix_ = "";
+	command_ = "";
+	params_ = "";
+	trailing_ = "";
+	target_ = "";
+	return *this;
+}
+
+ResponseBuilder &ResponseBuilder::prefix(const std::string &pref)
+{
+	prefix_ = pref;
+	return *this;
+}
+
+ResponseBuilder &ResponseBuilder::command(const std::string &cmd)
+{
+	command_ = cmd;
+	return *this;
+}
+
+ResponseBuilder &ResponseBuilder::params(const std::string &params)
+{
+	if (!params_.empty())
+		params_ += " ";
+	params_ += params;
+	return *this;
+}
+
+ResponseBuilder &ResponseBuilder::target(const std::string &target)
+{
+	target_ = target;
+	return *this;
+}
+
+ResponseBuilder &ResponseBuilder::trailing(const std::string &trailing)
+{
+	trailing_ = ":" + trailing;
+	return *this;
+}
+
+ResponseBuilder &ResponseBuilder::numeric(int code)
+{
+	std::ostringstream oss;
+	if (code < 10)
+	{
+		oss << "00" << code;
+	}
+	else if (code < 100)
+	{
+		oss << "0" << code;
+	}
+	oss << code;
+	command_ = oss.str();
+	return *this;
+}
+
+std::string ResponseBuilder::build()
+{
+	std::string result;
+
+	if (!prefix_.empty())
+		result += prefix_ + " ";
+	result += command_;
+
+	if (target_.empty())
+		result += " " + target_;
+
+	if (!params_.empty())
+		result += " " + params_;
+
+	if (!trailing_.empty())
+		result += " :" + trailing_;
+
+	result += "\r\n";
+
+	return result;
+}
