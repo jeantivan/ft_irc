dfb92fa065f313cd0fb63721efefa8bececa8da4
Jean Tivan <jptivan53@gmail.com>
2026-08-04 15:05:25 +0200

    ADD: Agrega o quita operadores del canal

--- src/Mode/OperatorMode.cpp
+++ src/Mode/OperatorMode.cpp
@@ -1,4 +1,7 @@
 #include "Mode/OperatorMode.hpp"
+#include "Client.hpp"
+#include "Server.hpp"
+#include "ResponseBuilder.hpp"
 
 OperatorMode::OperatorMode() : ModeHandler() {}
 
@@ -24,6 +27,49 @@ bool OperatorMode::requiresParam(bool isAdding) const
 
 bool OperatorMode::change(Channel *channel, bool isAdding, const std::string &param, Client *client, Server *server)
 {
+	if (param.empty())
+		return false;
 
-	return true;
+	const std::map<int, Client *> &members = channel->getMembers();
+	std::map<int, Client *>::const_iterator it;
+	Client *targetClient = NULL;
+
+	for (it = members.begin(); it != members.end(); ++it)
+	{
+		if (it->second->getNick() == param)
+		{
+			targetClient = it->second;
+			break;
+		}
+	}
+
+	if (!targetClient)
+	{
+		ResponseBuilder response;
+		response.prefix(server->getName())
+			.numeric(ERR_USERNOTINCHANNEL)
+			.target(client->getNick())
+			.params(param + " " + channel->getName())
+			.trailing("They aren't on that channel");
+		std::cout << "[ircserver]: Error ERR_USERNOTINCHANNEL " << response.build() << std::endl;
+
+		server->queueClientData(*client, response.build());
+		return false;
+	}
+
+	int targetFd = targetClient->getFd();
+	bool isCurrentlyOp = channel->isOperator(targetFd);
+
+	if (isAdding && !isCurrentlyOp)
+	{
+		channel->addOperator(targetFd);
+		return true;
+	}
+	else if (!isAdding && isCurrentlyOp)
+	{
+		channel->removeOperator(targetFd);
+		return true;
+	}
+
+	return false;
 }
